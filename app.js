dec4a370f622735c5f78fb28351f1ff2c585f03c
ranndy <ranndy180@gmail.com>
2024-08-04 16:18:42 -0500

    actualizacion 1

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 let numeroUsuario = 0;
-let numeroMaximoPosible = 20;
-let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
-
-//let veces = "vez";
+let numeroMaximoPosible = 8;
+//floor no redondea solo elimina la parte decimal 
+//Math.random() aroja numeros aleatorio pero decimales
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;//el numero (+1) no ayuda a sumarle una al resultado 
+//let veces = "vez";se remplaza por el operador ternario
 let intentos = 1;
-console.log(numeroSecreto);
-console.log(numeroUsuario);
-alert("hola, vuelvelo a intentar");
+console.log(`numeroUsuario es: ${numeroUsuario}, tipo de dato ${typeof (numeroUsuario)}`);
+console.log(`numeroSecreto es: ${numeroSecreto},tipo de dato ${typeof (numeroSecreto)}`);
+
 
 while(numeroUsuario != numeroSecreto ){
     //parseInt nos sirve para convetir un numero en un entero.
     numeroUsuario = parseInt(prompt(`indica un numero del 1 al ${numeroMaximoPosible}`));
-    console.log(typeof(numeroUsuario));
 
     if (numeroUsuario == numeroSecreto){
-        alert(`bien echo el numero es: ${numeroSecreto}, numero de intentos ${intentos} ${intentos == 1 ? "vez": "veces"}`);
-        //eces = "vez";
+        alert(`bien el numero es: ${numeroSecreto}, numero de intentos ${intentos} ${intentos == 1 ? "vez": "veces"}`);
+        //aveces = "vez"; se remplaza por el operador ternario
         intentos = 1;
     }else{
         if(numeroUsuario > numeroSecreto){
@@ -23,7 +23,7 @@ while(numeroUsuario != numeroSecreto ){
         }else{alert("el numero es mayor");}
         //intentos = intentos + 1;
         //intentos +=1;
-        //veces = "veces";
+        //veces = "veces";rempalzado por el operador ternario
         intentos ++;
         //cantidad de intentos.
         if(intentos >= 10){
